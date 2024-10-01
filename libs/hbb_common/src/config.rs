c7ba842fe0dce7f21c0fa84725901f0c74810c8a
shuzaigit <38025651+shuzaigit@users.noreply.github.com>
2024-10-01 11:47:22 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["www.fuxiaoer.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "T9MuSfoaz99wPn8K1aC4VHNjKNBUwpJE3MsAzzEyWCI=";
+pub const PUBLIC_RS_PUB_KEY: &str = "T8MuSfoaz99wPn8K1aC4VHNjKNBUwpJE3MsAzzEyWCI=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
